e107bf7e15f78244b07508be2c959d27fe244e37
Anant Jindal <anantjindal45@gmail.com>
2026-04-26 13:06:38 +0530

    Created connection with db

---
+++ db/conn.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/postgres'
+
+ENGINE = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=ENGINE)
+BASE = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+       yield db()
+    finally:
+        db.close();
+
+
+def create_db():
+    BASE.metadata.create_all(bind=ENGINE)
+    
