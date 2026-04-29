8a74e1ca8c9476017f4cd9c31aaf2df612629b9d
Anant Jindal <anantjindal45@gmail.com>
2026-04-29 09:26:35 +0530

    Added validation to check user in creating tasks

--- services/tasks_services.py
+++ services/tasks_services.py
@@ -2,9 +2,36 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from models.tasks_models import TASK_MODEL
 from schemas.tasks_schemas import TaskRequest
+from models.users_models import Users_Schema
 
 class TaskService:
-    
+
+    @staticmethod
+    def validate_user(data: TaskRequest, db: Session):
+        user_for_assignment= db.query(Users_Schema).filter(
+            Users_Schema.id == data.assigned_to
+        ).first()
+
+        if not user_for_assignment:
+            raise HTTPException(
+                status_code=404,
+                detail=f"User not found with {data.assigned_to}"
+            )
+
+        user_for_creation= db.query(Users_Schema).filter(
+            Users_Schema.id == data.created_by
+        ).first()
+
+        if not user_for_creation:
+            raise HTTPException(
+                status_code=404,
+                detail=f"User not found with {data.created_by}"
+            )
+        
+        return True
+
+
+
     @staticmethod
     def get_all_tasks(db: Session):
         return db.query(TASK_MODEL).all()
@@ -28,6 +55,9 @@ class TaskService:
     
     @staticmethod
     def create_task(data:TaskRequest, db:Session):
+
+        TaskService.validate_user(data, db)
+
         new_task = TASK_MODEL(**data.dict())
 
         len = db.query(TASK_MODEL).count()
@@ -40,6 +70,8 @@ class TaskService:
     
     @staticmethod
     def update_task(id: str, data: TaskRequest, db: Session):
+        TaskService.validate_user(data, db)
+
         task = TaskService.get_task_by_id(id, db)
 
         for key, value in data.dict().items():
